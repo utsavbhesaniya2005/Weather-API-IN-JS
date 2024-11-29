8102c71bff369e9b05af44bf520d68803144d796
utsavbhesaniya2005 <ukbhesaniya01@gmail.com>
2024-11-29 19:57:11 +0530

    All Uploaded

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ function getLocation(){
     fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${loc}?unitGroup=metric&key=NJQ3MSTFNA799ETMECM7PBUPL&contentType=json`)
     .then(res => res.json()).then(res1 => {
 
+        let dayForecast = res1.days;
+
         if(res1 && res1.address){
 
             document.getElementById('currentWeather').innerHTML = `
@@ -14,60 +16,53 @@ function getLocation(){
                         <p>Latitude : ${res1.latitude}</p>
                         <p>Longitude : ${res1.longitude}</p>
                     </div>
-                    <div class="weather-details">
-                        <img src="https://openweathermap.org/img/wn/01d.png" alt="Weather Icon" class="weather-icon">
-                        <h3 class="temperature">18°C</h3>
-                        <p class="description">Clear Sky</p>
-                        <p>Humidity: 55%</p>
-                        <p>Wind Speed: 10 km/h</p>
+                    <div class="weather-details" id="currentForecast">
                     </div>
                 </div>
 
                 <div class="forecast">
-                    <h2>7-Day Forecast</h2>
-                    <div class="forecast-container">
-                        <div class="forecast-day">
-                            <h4>Mon</h4>
-                            <img src="https://openweathermap.org/img/wn/02d.png" alt="Cloudy Icon">
-                            <p>15°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Tue</h4>
-                            <img src="https://openweathermap.org/img/wn/03d.png" alt="Partly Cloudy Icon">
-                            <p>17°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Wed</h4>
-                            <img src="https://openweathermap.org/img/wn/04d.png" alt="Cloudy Icon">
-                            <p>16°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Thu</h4>
-                            <img src="https://openweathermap.org/img/wn/09d.png" alt="Rain Icon">
-                            <p>14°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Fri</h4>
-                            <img src="https://openweathermap.org/img/wn/10d.png" alt="Rain Icon">
-                            <p>13°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Sat</h4>
-                            <img src="https://openweathermap.org/img/wn/11d.png" alt="Thunderstorm Icon">
-                            <p>12°C</p>
-                        </div>
-                        <div class="forecast-day">
-                            <h4>Sun</h4>
-                            <img src="https://openweathermap.org/img/wn/13d.png" alt="Snow Icon">
-                            <p>5°C</p>
-                        </div>
+                    <h2>Last 15-Day Weather Forecast</h2>
+                    <div class="forecast-container" id="weeklyForecast">
+                        
                     </div>
                 </div>
             `;
+
+             
+
+            dayForecast.forEach(day => {
+
+                let currentDate = new Date().toISOString().split('T')[0];
+
+                let getCurrentDate = day.datetime;
+
+                if(getCurrentDate === currentDate){
+                    document.getElementById('currentForecast').innerHTML = `
+                        <img src="./images/${day.icon}.png" alt="${day.conditions}" class="weather-icon">
+                        <h3 class="temperature">${day.temp}°C</h3>
+                        <p class="description">${day.conditions}</p>
+                        <p>Humidity: ${day.humidity}%</p>
+                        <p>Wind Speed: ${day.windspeed} km/h</p>
+                    `;
+                }
+            });
+
+            let weeklyData = res1.days;
+            weeklyData.forEach(day => {
+                
+                document.getElementById('weeklyForecast').innerHTML += `
+                    <div class="forecast-day">
+                        <h4>${day.datetime}</h4>
+                        <img src="./images/${day.icon}.png" alt="${day.conditions}">
+                        <p>${day.temp}°C</p>
+                    </div>
+                `;
+            });
+
         }
     })
     .catch(error => {
-        console.log(error);
+        document.getElementById('currentWeather').innerHTML = `<h1 style="color : black;font-size : 52px;">Data Cannot Be Found.</h1>`
     })
 }
 getLocation();
